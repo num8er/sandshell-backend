075ec29bc51f5539be7824ae11e4f701281fd9ca
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2025-02-06 01:40:42 +0000

    landing page, nginx config, port change

--- config/dev.exs
+++ config/dev.exs
@@ -10,7 +10,7 @@ config :sandshell_api, SandshellApi.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   server: true,
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 5500],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :sandshell_api, SandshellApi.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   server: true,
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 5500],
   url: [host: "sandshell.dev", port: 80],
   check_origin: false,
   code_reloader: true,
